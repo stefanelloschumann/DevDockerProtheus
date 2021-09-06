3949d130349c4b1e04bc0d6c1a703affa2dcea99
Houston Santos <houston_santos@hotmail.com>
2021-09-06 19:29:46 -0300

    update entrypoint-postgres.sh

--- build/postgres/entrypoint-postgresql.sh
+++ build/postgres/entrypoint-postgresql.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 # Adicionando configurações aos arquivos .ini
+/bin/sed 's/{{DATABASE}}/'"${DATABASE}"'/' -i /etc/odbc.ini
 /bin/sed 's/{{POSTGRES}}/'"${TOPALIAS}"'/' -i /etc/odbc.ini
+/bin/sed 's/{{DB_USER}}/'"${DB_USER}"'/' -i /etc/odbc.ini
+/bin/sed 's/{{DB_PASSWORD}}/'"${DB_PASSWORD}"'/' -i /etc/odbc.ini
 /bin/sed 's/{{POSTGRES}}/'"${TOPALIAS}"'/' -i /srv/totvs/dbaccess/dbaccess.ini
 /bin/sed 's/{{IP_APPSERVER}}/'"${IP_APPSERVER}"'/' -i /srv/totvs/dbaccess/dbaccess.ini
+
+./dbaccesscfg -u ${DB_USER} -p ${DB_PASSWORD} -d ${DATABASE} -a ${TOPALIAS}
 
 # Iniciando serviços postgres e dbaccess
 /docker-entrypoint.sh postgres & cd /srv/totvs/dbaccess && ./dbaccess64
